c0a71e8038dec7615d332602584a05b861405d39
Charlie Joiner <charlie.joiner.29@gmail.com>
2023-02-21 22:40:04 +0000

    praying rn

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -11,7 +11,10 @@ function Home() {
 					<h1 className="text-4xl font-semibold flex-auto">
 						Welcome to PubGolf!
 					</h1>
-					<img src={'/src/media/pubgolf.png'} className="w-10 h-10 flex-auto" />
+					<img
+						src={'../../media/pubgolf.png'}
+						className="w-10 h-10 flex-auto"
+					/>
 				</div>
 			</div>
 			<h2 className="text-lg mx-11 mt-3 bg-home-color rounded-md w-screen- p-3 leading-relaxed text-neutral-200">
