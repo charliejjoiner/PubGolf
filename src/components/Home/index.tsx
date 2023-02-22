2b1bee63f5a97058a550fbf5140c233eda8e6d44
Charlie Joiner <charlie.joiner.29@gmail.com>
2023-02-22 15:29:36 +0000

    ReStyle

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -7,7 +7,7 @@ function Home() {
 	return (
 		<body>
 			<Navbar />
-			<div className="w-fit mx-11 mt-11">
+			<div className="w-fit mx-11 mt-5">
 				<div className="rounded-md bg-dark-color h-12 text-center h-full p-3 text-light-color flex">
 					<h1 className="text-4xl font-semibold flex-auto">
 						Welcome to PubGolf!
@@ -15,12 +15,14 @@ function Home() {
 					<img src={PubGolf} className="w-10 h-10 flex-auto" />
 				</div>
 			</div>
-			<h2 className="text-lg mx-11 mt-3 bg-home-color rounded-md w-screen- p-3 leading-relaxed text-neutral-200">
-				If you've never played the game - Why not try it sometime?
-				<br /> There are lots of variation on the rules of the game and the
-				number of holes played but in general the game consists of several pubs,
-				as well as quite a lot of beer, spirits and a bit of walking.
-			</h2>
+			<div className="mt-5 bg-zinc-900/50 p-3 w-screen">
+				<h2 className="pl-10 text-lg leading-relaxed text-neutral-100">
+					If you've never played the game - Why not try it sometime?
+					<br /> There are lots of variation on the rules of the game and the
+					number of holes played but in general the game consists of several
+					pubs, as well as quite a lot of beer, spirits and a bit of walking.
+				</h2>
+			</div>
 			<Footer />
 		</body>
 	);
--- src/components/Scorecard/index.tsx
+++ src/components/Scorecard/index.tsx
@@ -15,28 +15,28 @@ function Scorecard() {
 					</div>
 				</div>
 
-				<div className="w-fit w-full">
+				<div className="w-2/4 w-full">
 					<div className="mx-11 mt-3 bg-home-color rounded-md w-screen- p-3 leading-relaxed text-neutral-200">
-						<h2 className="text-xl underline">
+						<h2 className="text-lg underline">
 							Highlight and Copy this scorecard into your notes app on your
 							phone, you can then edit it to your specific game.
 						</h2>
 					</div>
 				</div>
-				<div className="w-fit text-center content-center">
-					<table className="mt-3 w-screen table-auto bg-emerald-200 rounded-md border border-slate-500">
+				<div className="text-center content-center">
+					<table className="table-auto mt-3 ml-12 bg-emerald-200 border border-slate-500">
 						<thead className="border border-slate-500">
 							<tr>
-								<td className="border-r border-slate-500">Hole</td>
-								<td className="border-r border-slate-500">Yardage</td>
-								<td className="border-r border-slate-500">Water Hazard</td>
-								<td className="border-r border-slate-500">Par</td>
-								<td>Score</td>
+								<td className="border-r border-slate-500 px-24 py-2">Hole</td>
+								<td className="border-r border-slate-500 px-8">Yardage</td>
+								<td className="border-r border-slate-500 px-1">Water Hazard</td>
+								<td className="border-r border-slate-500 px-2">Par</td>
+								<td className="px-2">Score</td>
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
-								<td className="border-r border-slate-500">Pub1</td>
+								<td className="border-r border-slate-500">Thomas Ingoldsby</td>
 								<td className="border-r border-slate-500">1 Beer</td>
 								<td className="border-r border-slate-500">N</td>
 								<td className="border-r border-slate-500">4</td>
@@ -51,7 +51,7 @@ function Scorecard() {
 							</tr>
 							<tr>
 								<td className="border-r border-slate-500">Pub3</td>
-								<td className="border-r border-slate-500"></td>
+								<td className="border-r border-slate-500">Rum and Coke</td>
 								<td className="border-r border-slate-500">Y</td>
 								<td className="border-r border-slate-500">4</td>
 								<td className="border-r border-slate-500"></td>
